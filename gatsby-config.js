89a4abdd755578b35a8a45e7ac14a53d405be6c5
dramxx <drahoslav.madar@gmail.com>
2021-01-12 22:09:35 +0100

    refactoring

--- gatsby-config.js
+++ gatsby-config.js
@@ -140,13 +140,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `gatsby-wordpress-demo`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/DMA.jpg`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        marginTop: `2rem`,
+      }}
+    >
+      footer
+    </footer>
+  )
+}
+
+export default Footer
