411a29067067b0cfb208cad2c03249ffd649fd1f
drahoslav.madar@aliter.com <drahoslav.madar@gmail.com>
2021-01-27 11:28:37 +0100

    removed footer border

--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -3,7 +3,6 @@ import styled from "styled-components"
 export const StyledFooter = styled.footer`
   display: flex;
   padding: 2rem 1rem;
-  border-top: 3px solid #333;
   position: absolute;
   bottom: 0;
 `
